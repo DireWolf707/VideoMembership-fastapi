2f7ed45ad3e9964119f3cf8b6862f74d023760b6
DireWolf707 <rahulsood707@gmail.com>
2021-12-27 00:55:54 +0530

    first commit

---
+++ app/config.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings,Field
+from pathlib import WindowsPath
+from functools import lru_cache
+import pathlib
+
+BASE_DIR = ASTRADB_CONNECT_BUNDLE = pathlib.Path(__file__).parent
+
+class Settings(BaseSettings):
+    keyspace: str = Field(...,env="ASTRADB_KEYSPACE")
+    base_dir: WindowsPath = BASE_DIR
+    db_client_id:str = Field(...,env="ASTRADB_CLIENT_ID")
+    db_client_secret:str = Field(...,env="ASTRADB_CLIENT_SECRET")
+
+    class Config:
+        env_file = BASE_DIR.parent / '.env'
+
+@lru_cache
+def get_settings():
+    return Settings()
---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from . import config
+
+settings = config.get_settings()
+app = FastAPI()
+
+@app.get("/")
+def homepage():
+    return {"hello":settings.keyspace}
